7c31926c590948673f4c44aa6e3c607a366593eb
dilmurod <dilmurod@kali.dilmurod>
2025-01-30 09:08:58 +0500

    students1

--- main.py
+++ main.py
@@ -4,4 +4,30 @@ from tinydb import TinyDB, Query
 db = TinyDB('students.json')
 
 # Reference the default table
-students_table = db.table('students')
+students1 = {
+    "id": 101,
+    "name": "John Doe",
+    "age": 16,
+    "gender": "Male",
+    "contact": "123-456-7890",
+    "grade_level": "Grade 10",
+    "subjects": {
+        "math": 85,
+        "science": 90,
+        "english": 88
+    },
+    "attendance": 92.5,
+    "activities": ["Basketball", "Debate Club"],
+    "address": {
+        "street": "123 Main St",
+        "city": "Springfield",
+        "state": "IL",
+        "zip_code": "62704"
+    }
+}
+
+
+
+
+
+
